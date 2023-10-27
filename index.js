5d810c26f870256d923afc0da83d21a23a45068e
sytwbx <123071744+sytwbx@users.noreply.github.com>
2023-10-27 12:34:38 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-document.querySelector(".img").addEventListener("click" , function (){
+var img = document.querySelector(".img2")
+
+img.addEventListener("click" , function (){
     var rn = "image/"+ Math.floor(Math.random() * 9) + ".png";
-    document.querySelector("img").src = rn;
+    img.src = rn;
 });
-document.onkeydown= function(){
-    var rn = "image/"+ Math.floor(Math.random() * 9) + ".png";
-    document.querySelector("img").src = rn;
-}
+// document.onkeydown= function(){
+//     var rn = "image/"+ Math.floor(Math.random() * 9) + ".png";
+//     document.querySelector(".img2").src = rn;
+// }
 
 
 
