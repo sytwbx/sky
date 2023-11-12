7173924b8afb315fdddc91fe4f1177dbe9b253f7
sytwbx <123071744+sytwbx@users.noreply.github.com>
2023-11-12 12:40:30 +0800

    Add files via upload

--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,31 @@
 var img = document.querySelector(".img2");
 var sound = document.querySelector(".sound");
 var audio = document.getElementById("bgMusic");
+var shang = document.querySelector(".fanye .shang");
+var xia = document.querySelector(".xia")
+var dian = document.querySelector(".fanye").querySelectorAll("p")[count + 1];
+var count = 0;
 
-audio.play();
+shang.addEventListener("click" , function (){
+    if (count == 0){
+        count = 10;
+        img.src =  "image/"+ count + ".png";
+        dian.innerText="0";
+        dian.classList.add("dadian");
+    }else{
+        count -= 1
+        img.src = "image/"+ count + ".png";
+    }
+});
 
-img.addEventListener("click" , function (){
-    var rn = "image/"+ Math.floor(Math.random() * 11) + ".png";
-    img.src = rn;
+xia.addEventListener("click" , function (){
+    if (count == 10){
+        count = 0;
+        img.src =  "image/"+ count + ".png";
+    }else{
+        count += 1;
+        img.src = "image/"+ count + ".png";
+    }
 });
 
 sound.addEventListener("click", function(){
